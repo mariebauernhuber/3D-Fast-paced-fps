e2ac96f6735acd3b0b064f2036c8a73c7ba297fb
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-10 17:44:27 +0100

    Code rework to not use SDL_MAIN_CALLBACKS

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 #include <vector>
 #include <cmath>
 #include <iostream>
@@ -735,11 +733,11 @@ void RenderObject(SDL_Renderer* renderer, const Object3D &obj, const mat4x4 &mat
 }
 
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
+int main(int argc, char* argv[]){
     if(!SDL_Init(SDL_INIT_VIDEO)){
-        return SDL_APP_FAILURE;
+        return 1;
     }
-    window = SDL_CreateWindow("Test", 1280, 720, SDL_WINDOW_RESIZABLE | SDL_WINDOW_MOUSE_RELATIVE_MODE | SDL_WINDOW_MOUSE_GRABBED);
+    window = SDL_CreateWindow("Amazing game", 1280, 720, SDL_WINDOW_RESIZABLE | SDL_WINDOW_MOUSE_RELATIVE_MODE | SDL_WINDOW_MOUSE_GRABBED);
     renderer = SDL_CreateRenderer(window, NULL);
 
     CalculateScreenTransforms();
@@ -797,153 +795,151 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
 
     std::cout << "Init done!";
 
-    return SDL_APP_CONTINUE;
-}
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
-    if(event->type == SDL_EVENT_QUIT) { return SDL_APP_SUCCESS; }
+    bool is_running = true;
+    SDL_Event event;
 
-    if(event->type == SDL_EVENT_WINDOW_RESIZED){
-        CalculateScreenTransforms();
-        CalculateScreenProjection();
-    }
+    while(is_running){
+	    while(SDL_PollEvent(&event)){
+		    if (event.type == SDL_EVENT_QUIT) {
+			    is_running = false;
+		    }
+		    if(event.type == SDL_EVENT_WINDOW_RESIZED){
+			CalculateScreenTransforms();
+			CalculateScreenProjection();
+		    }
 
-    if(event->type == SDL_EVENT_KEY_DOWN){
-        if(event->key.scancode == SDL_SCANCODE_ESCAPE){ return SDL_APP_SUCCESS; }
-        if(event->key.scancode == SDL_SCANCODE_F11){
-            if(SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN){
-                SDL_SetWindowFullscreen(window, 0);
-            }else{
-                SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-            };
-            
-        };
-        if(event->key.scancode == SDL_SCANCODE_F8) {
-            debugModeTogggled = !debugModeTogggled;
-        };
-        if (event->key.scancode == SDL_SCANCODE_F6) {
-            if (gCullMode == CullMode::Back) gCullMode = CullMode::Front;
-            else if (gCullMode == CullMode::Front) gCullMode = CullMode::None;
-            else gCullMode = CullMode::Back;
-        }
+		    if(event.type == SDL_EVENT_KEY_DOWN){
+			if(event.key.scancode == SDL_SCANCODE_ESCAPE){ is_running = false; }
+			if(event.key.scancode == SDL_SCANCODE_F11){
+			    if(SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN){
+				SDL_SetWindowFullscreen(window, 0);
+			    }else{
+				SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+			    };
+			    
+			};
+			if(event.key.scancode == SDL_SCANCODE_F8) {
+			    debugModeTogggled = !debugModeTogggled;
+			};
+			if (event.key.scancode == SDL_SCANCODE_F6) {
+			    if (gCullMode == CullMode::Back) gCullMode = CullMode::Front;
+			    else if (gCullMode == CullMode::Front) gCullMode = CullMode::None;
+			    else gCullMode = CullMode::Back;
+			}
 
-    }
+		    }
 
-    if(event->type == SDL_EVENT_MOUSE_MOTION){
-        fYaw   += event->motion.xrel * fMouseSensitivity;
-        fPitch -= event->motion.yrel * fMouseSensitivity;
+		    if(event.type == SDL_EVENT_MOUSE_MOTION){
+			fYaw   += event.motion.xrel * fMouseSensitivity;
+			fPitch -= event.motion.yrel * fMouseSensitivity;
 
-        // Clamp pitch to avoid flipping
-        if (fPitch > fMaxPitch)  fPitch = fMaxPitch;
-        if (fPitch < -fMaxPitch) fPitch = -fMaxPitch;
-    }
+			// Clamp pitch to avoid flipping
+			if (fPitch > fMaxPitch)  fPitch = fMaxPitch;
+			if (fPitch < -fMaxPitch) fPitch = -fMaxPitch;
+		    }
 
-    return SDL_APP_CONTINUE;
-}
+	    }
 
-SDL_AppResult SDL_AppIterate(void *appstate){
-    // 1. Update delta time
-    CalculateDeltaTime();
-    MinuteTimer();
+		    // 1. Update delta time
+		    CalculateDeltaTime();
+		    MinuteTimer();
 
-    // 2. Limit frame rate
-    float remainingFrameTime = (1.0f / targetFrameRate) - deltaTime;
-    if (remainingFrameTime > 0.001f){
-        SDL_Delay(int(remainingFrameTime * 1000.0f));
-    }
+		    // 2. Limit frame rate
+		    float remainingFrameTime = (1.0f / targetFrameRate) - deltaTime;
+		    if (remainingFrameTime > 0.001f){
+			SDL_Delay(int(remainingFrameTime * 1000.0f));
+		    }
 
-    // 3. Process keyboard input for camera movement
-    const bool *key_states = SDL_GetKeyboardState(NULL);
-    float fMoveSpeed = 8.0f * deltaTime;
+		    // 3. Process keyboard input for camera movement
+		    const bool *key_states = SDL_GetKeyboardState(NULL);
+		    float fMoveSpeed = 8.0f * deltaTime;
+			
+		    // Forward / Back
+		    if (key_states[SDL_SCANCODE_W])
+			vCamera = Vector_Add(vCamera, Vector_Mul(Vector_Normalise({vLookDir.x, 0.0f, vLookDir.z}), fMoveSpeed));
+		    if (key_states[SDL_SCANCODE_S])
+			vCamera = Vector_Sub(vCamera, Vector_Mul(Vector_Normalise({vLookDir.x, 0.0f, vLookDir.z}), fMoveSpeed));
 
-    // Forward / Back
-    if (key_states[SDL_SCANCODE_W])
-        vCamera = Vector_Add(vCamera, Vector_Mul(Vector_Normalise({vLookDir.x, 0.0f, vLookDir.z}), fMoveSpeed));
-    if (key_states[SDL_SCANCODE_S])
-        vCamera = Vector_Sub(vCamera, Vector_Mul(Vector_Normalise({vLookDir.x, 0.0f, vLookDir.z}), fMoveSpeed));
+		    // Right / Left
+		    vec3d vRight = Vector_Normalise(Vector_CrossProduct(vLookDir, {0,1,0}));
+		    if (key_states[SDL_SCANCODE_D])
+			vCamera = Vector_Sub(vCamera, Vector_Mul(vRight, fMoveSpeed));
+		    if (key_states[SDL_SCANCODE_A])
+			vCamera = Vector_Add(vCamera, Vector_Mul(vRight, fMoveSpeed));
 
-    // Right / Left
-    vec3d vRight = Vector_Normalise(Vector_CrossProduct(vLookDir, {0,1,0}));
-    if (key_states[SDL_SCANCODE_D])
-        vCamera = Vector_Sub(vCamera, Vector_Mul(vRight, fMoveSpeed));
-    if (key_states[SDL_SCANCODE_A])
-        vCamera = Vector_Add(vCamera, Vector_Mul(vRight, fMoveSpeed));
+		    // Up / Down
+		    if (key_states[SDL_SCANCODE_SPACE])
+			vCamera.y += 8.0f * deltaTime;
+		    if (key_states[SDL_SCANCODE_LSHIFT])
+			vCamera.y -= 8.0f * deltaTime;
 
-    // Up / Down
-    if (key_states[SDL_SCANCODE_SPACE])
-        vCamera.y += 8.0f * deltaTime;
-    if (key_states[SDL_SCANCODE_LSHIFT])
-        vCamera.y -= 8.0f * deltaTime;
+		    // 4. Update camera orientation based on mouse look
+		    vec3d vForward = {
+			cosf(fPitch) * sinf(fYaw),
+			sinf(fPitch),
+			cosf(fPitch) * cosf(fYaw)
+		    };
 
-    // 4. Update camera orientation based on mouse look
-    vec3d vForward = {
-        cosf(fPitch) * sinf(fYaw),
-        sinf(fPitch),
-        cosf(fPitch) * cosf(fYaw)
-    };
+		    vLookDir = Vector_Normalise(vForward);
 
-    vLookDir = Vector_Normalise(vForward);
+		    objects[2].rotation.z += 2.0f * deltaTime;
+		    objects[1].rotation.y += 2.0f * deltaTime;
+		    objects[0].rotation.x += 2.0f * deltaTime;
 
-    objects[2].rotation.z += 2.0f * deltaTime;
-    objects[1].rotation.y += 2.0f * deltaTime;
-    objects[0].rotation.x += 2.0f * deltaTime;
+		    objects[4].rotation.y += 2.0f * deltaTime;
 
-    objects[4].rotation.y += 2.0f * deltaTime;
+		    // 5. Update view matrix
+		    vec3d vUp = {0, 1, 0};
+		    matView = Matrix_PointAt(vCamera, Vector_Add(vCamera, vLookDir), vUp);
+		    matView = Matrix_QuickInverse(matView);
 
-    // 5. Update view matrix
-    vec3d vUp = {0, 1, 0};
-    matView = Matrix_PointAt(vCamera, Vector_Add(vCamera, vLookDir), vUp);
-    matView = Matrix_QuickInverse(matView);
+		    UpdateFrustumPlanes();
 
-    UpdateFrustumPlanes();
+		    // 6. Clear screen
+		    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		    SDL_RenderClear(renderer);
+		    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    // 6. Clear screen
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		    // 7. Render all objects
+		    for(auto &obj : objects){
+			RenderObject(renderer, obj, matView, matProj);
+		    }
 
-    // 7. Render all objects
-    for(auto &obj : objects){
-        RenderObject(renderer, obj, matView, matProj);
-    }
+		    if(consoleOpen){
+		    // Draw a semi-transparent background
+		    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 180); 
+		    SDL_FRect consoleRect = {50, 50, 400, 300};
+		    SDL_RenderFillRect(renderer, &consoleRect);
 
-    if(consoleOpen){
-    // Draw a semi-transparent background
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 180); 
-    SDL_FRect consoleRect = {50, 50, 400, 300};
-    SDL_RenderFillRect(renderer, &consoleRect);
+		    // Here you could draw text using SDL_ttf or something simple
+		    // For debugging, you could also print object positions to console
+		    for(size_t i = 0; i < objects.size(); i++){
+			std::cout << "Object " << i 
+				  << " Pos: " << objects[i].position.x << ", " 
+				  << objects[i].position.y << ", " 
+				  << objects[i].position.z << "\n";
+			std::cout << "Rot: " << objects[i].rotation.x << ", "
+				  << objects[i].rotation.y << ", "
+				  << objects[i].rotation.z << "\n";
+		    }
+		}
 
-    // Here you could draw text using SDL_ttf or something simple
-    // For debugging, you could also print object positions to console
-    for(size_t i = 0; i < objects.size(); i++){
-        std::cout << "Object " << i 
-                  << " Pos: " << objects[i].position.x << ", " 
-                  << objects[i].position.y << ", " 
-                  << objects[i].position.z << "\n";
-        std::cout << "Rot: " << objects[i].rotation.x << ", "
-                  << objects[i].rotation.y << ", "
-                  << objects[i].rotation.z << "\n";
-    }
-}
+		    // 8. Present final frame
+		    SDL_RenderPresent(renderer);
 
-    // 8. Present final frame
-    SDL_RenderPresent(renderer);
+		    // 9. Update real frame rate
+		    realFrameRate = 1.0f / deltaTime;
 
-    // 9. Update real frame rate
-    realFrameRate = 1.0f / deltaTime;
+		    // 10. Optional debug info
+		    if(debugModeTogggled)
+			PrintDebugInfo();
 
-    // 10. Optional debug info
-    if(debugModeTogggled)
-        PrintDebugInfo();
-
-    // 11. Increment draw cycles
-    nDrawCycles++;
-
-    return SDL_APP_CONTINUE;
-}
-
-
-void SDL_AppQuit(void *appstate, SDL_AppResult result){
+		    // 11. Increment draw cycles
+		    nDrawCycles++;
+	    }
     SDL_Quit();
+    return 0;
 }
 
